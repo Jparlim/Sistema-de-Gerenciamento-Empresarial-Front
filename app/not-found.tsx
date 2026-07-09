2c2ab3c66d950d560d5392989715e1e6074a0dea
Jparlim <gusjao0500@gmail.com>
2026-07-09 10:07:51 -0300

    creating not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="h-screen border-2 flex justify-center items-center">
+      <h1 className="w-max mb-40 border-b-2 font-extrabold text-[60px]">
+        página não encontrada
+      </h1>
+    </div>
+  );
+}
