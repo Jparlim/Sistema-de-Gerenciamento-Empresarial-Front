7790d8f29a4fb4ecafa1252da002e2c69941f41e
Jparlim <gusjao0500@gmail.com>
2026-07-20 11:02:19 -0300

    creating the page create account

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,78 @@
-import Image from "next/image";
+import logo from "@/public/Logo.png";
+import lock from "@/public/Lock.png";
+import { Metadata } from "next";
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: "Quantyum - Gerencia | Analise | Evolua",
+  description: "Todos os dados da sua empresa em um só lugar",
+  openGraph: {
+    title: "Quantyum - Sistema para sua empresa",
+    description: "Todos os dados da sua empresa em um só lugar",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+};
+
+export default function CreateCount() {
+  const teste = 2 * 1;
+  const teste2 = true;
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main>
+      <div className="flex flex-col gap-4">
+        <h1>Criar conta</h1>
+        <p className="text-[20px]">
+          Antes de continuar, confirme seu email para receber o token de
+          ativação
+        </p>
+
+        <div className="flex flex-col gap-1">
+          <div className="flex justify-between mx-2">
+            <p className="text-[26px] font-semibold">Passo 1</p>
+            {!teste ? (
+              <p className="text-[26px] font-semibold">Passo 2</p>
+            ) : (
+              <p className="text-[26px] text-gray-400 font-semibold">Passo 2</p>
+            )}
+          </div>
+          <div className={`w-full h-5 rounded-[20px] bg-gra`}>
+            <div className={`w-[${teste}px] h-5 rounded-[20px] bg-blu`} />
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="flex flex-col gap-4">
+          <div>
+            <h3>Nome da empresa</h3>
+            <input type="text" placeholder="" />
+          </div>
+          <div>
+            <h3>Email</h3>
+            <input type="text" placeholder="exemplo@gmail.com" />
+          </div>
+          <div>
+            <h3>Nome da empresa</h3>
+            <input type="text">
+              <img src={lock.src} alt="" />
+            </input>
+          </div>
+          <div>
+            <h3>Nome da empresa</h3>
+            <input type="text" />
+          </div>
+          <div>
+            <h3>Nome da empresa</h3>
+            <input type="text" />
+          </div>
+          <button>Enviar token</button>
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
