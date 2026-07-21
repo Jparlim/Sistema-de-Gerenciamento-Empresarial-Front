9f0b3ab9ef54b567df289532ccd49d9ccda8bf2e
Jparlim <Gusjao0500@gmail.com>
2026-07-21 20:16:43 -0300

    starting the connection between front and back end

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,9 @@ export const metadata: Metadata = {
   },
 };
 
+// ("use client");
+// ("use server");
+
 export default function CreateCount() {
   const teste = 2 * 1;
   const teste2 = true;
@@ -37,7 +40,7 @@ export default function CreateCount() {
         <div className="flex flex-col gap-1">
           <div className="flex justify-between mx-2">
             <p className="text-[26px] font-semibold">Passo 1</p>
-            {!teste ? (
+            {!teste2 ? (
               <p className="text-[26px] font-semibold">Passo 2</p>
             ) : (
               <p className="text-[26px] text-gray-400 font-semibold">Passo 2</p>
@@ -51,32 +54,33 @@ export default function CreateCount() {
         <div className="flex flex-col gap-4">
           <div className="">
             <h3>Nome da empresa</h3>
-            <input type="text" placeholder="" className="w-full py-2" />
+            <input type="text" placeholder="" className="w-full py-2 pl-2" />
           </div>
           <div>
             <h3>Email</h3>
-            <input type="text" className="w-full py-2" />
+            <input type="text" className="w-full py-2 pl-2" />
           </div>
           <div className="relative">
             <h3>Senha</h3>
-            <input type="text" className="w-full py-2" />
+            <input type="text" className="w-full py-2 pl-11" />
             <img
               src={lock.src}
-              width={28}
-              height={28}
-              className="absolute top-11 left-2"
+              width={25}
+              height={25}
+              className="absolute top-[46px] left-3 cursor-pointer"
+              // onClick={}
             />
           </div>
           <div>
             <h3>Telefone</h3>
-            <input type="text" className="w-full py-2" />
+            <input type="text" className="w-full py-2 pl-2" />
           </div>
-          <div className="flex justify-between">
+          <div className="flex justify-between items-end">
             <div className="w-[60%]">
               <h3>CNPJ</h3>
-              <input type="text" className="w-full py-2" />
+              <input type="text" className="w-full py-2 pl-2" />
             </div>
-            <button className="w-[30%]">Enviar token</button>
+            <button className="w-[30%] h-12">Enviar token</button>
           </div>
         </div>
       </div>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["172.31.192.1"],
+  allowedDevOrigins: ["172.31.192.1", "26.150.197.33"],
 };
 
 export default nextConfig;
